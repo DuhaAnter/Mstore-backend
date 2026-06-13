12bc26de1453b52b3cfdc0d4c948d9d2bbc32516
Duha <duhaanter@gmail.com>
2026-06-13 15:50:31 +0300

    deploying on vercel

--- src/server.js
+++ src/server.js
@@ -14,27 +14,27 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 // Import routes
-const productRoutes = require('./routes/products');
-const userRoutes = require ('./routes/users.js');
-const categoryRoutes =require('./routes/categories.js');
-const subCategoryRoutes =require('./routes/subCategories.js');
-const cartRoutes = require('./routes/cart.js');
-const orderRoutes = require('./routes/order.js');
-const couponRoutes = require ('./routes/coupon.js');
-const reviewRoutes = require('./routes/review.js');
+// const productRoutes = require('./routes/products');
+// const userRoutes = require ('./routes/users.js');
+// const categoryRoutes =require('./routes/categories.js');
+// const subCategoryRoutes =require('./routes/subCategories.js');
+// const cartRoutes = require('./routes/cart.js');
+// const orderRoutes = require('./routes/order.js');
+// const couponRoutes = require ('./routes/coupon.js');
+// const reviewRoutes = require('./routes/review.js');
 // main route
 app.get('/', (req, res) => {
   res.json({ message: 'M-Store Backend is running' });
 });
 // Use routes
-app.use('/products', productRoutes);
-app.use('/users', userRoutes);
-app.use('/categories' , categoryRoutes);
-app.use('/subcategories' , subCategoryRoutes);
-app.use('/cart',cartRoutes);
-app.use('/order',orderRoutes);
-app.use('/coupons',couponRoutes);
-app.use('/reviews',reviewRoutes);
+// app.use('/products', productRoutes);
+// app.use('/users', userRoutes);
+// app.use('/categories' , categoryRoutes);
+// app.use('/subcategories' , subCategoryRoutes);
+// app.use('/cart',cartRoutes);
+// app.use('/order',orderRoutes);
+// app.use('/coupons',couponRoutes);
+// app.use('/reviews',reviewRoutes);
 
 
 // app.listen(PORT, () => {
