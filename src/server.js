1131d70921269afe9d26a55b8312dac0a3d3dc8f
Duha <duhaanter@gmail.com>
2026-06-13 15:39:55 +0300

    deploying on vercel

--- src/server.js
+++ src/server.js
@@ -37,6 +37,8 @@ app.use('/coupons',couponRoutes);
 app.use('/reviews',reviewRoutes);
 
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+module.exports=app;
