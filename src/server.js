a7b4af727e06644c75aab05abd293901223451fc
Duha <duhaanter@gmail.com>
2026-06-20 15:44:07 +0300

    deploying on vercel

--- src/server.js
+++ src/server.js
@@ -2,43 +2,54 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
+
 dotenv.config();
 
 const app = express();
-// const PORT = process.env.PORT ;
 
-// app.use(cors({
-//   origin: 'http://localhost:5173', // Your exact React/Vite frontend URL (no trailing slash!)
-//   credentials: true                // Allowed to receive cookies from the frontend
-// }));
+const PORT = process.env.PORT;   
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'https://mstore-frontend-two.vercel.app/'   
+    : 'http://localhost:5173',
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
+
 // Import routes
-// const productRoutes = require('./src/routes/products.js');
-// const userRoutes = require ('./src/routes/users.js');
-// const categoryRoutes =require('./src/routes/categories.js');
-// const subCategoryRoutes =require('./src/routes/subCategories.js');
-// const cartRoutes = require('./src/routes/cart.js');
-// const orderRoutes = require('./src/routes/order.js');
-// const couponRoutes = require ('./src/routes/coupon.js');
-// const reviewRoutes = require('./src/routes/review.js');
-// main route
+const productRoutes = require('./routes/products.js');
+const userRoutes = require('./routes/users.js');
+const categoryRoutes = require('./routes/categories.js');
+const subCategoryRoutes = require('./routes/subCategories.js');
+const cartRoutes = require('./routes/cart.js');
+const orderRoutes = require('./routes/order.js');
+const couponRoutes = require('./routes/coupon.js');
+const reviewRoutes = require('./routes/review.js');
+
+// Main route
 app.get('/', (req, res) => {
   res.json({ message: 'M-Store Backend is running' });
 });
+
 // Use routes
-// app.use('/products', productRoutes);
-// app.use('/users', userRoutes);
-// app.use('/categories' , categoryRoutes);
-// app.use('/subcategories' , subCategoryRoutes);
-// app.use('/cart',cartRoutes);
-// app.use('/order',orderRoutes);
-// app.use('/coupons',couponRoutes);
-// app.use('/reviews',reviewRoutes);
+app.use('/products', productRoutes);
+app.use('/users', userRoutes);
+app.use('/categories', categoryRoutes);
+app.use('/subcategories', subCategoryRoutes);
+app.use('/cart', cartRoutes);
+app.use('/order', orderRoutes);
+app.use('/coupons', couponRoutes);
+app.use('/reviews', reviewRoutes);
 
+// === FOR VERCEL ===
+if (process.env.NODE_ENV !== 'production') {
+  //run listen() 
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-module.exports= app;
+module.exports = app;   // Vercel needs this
